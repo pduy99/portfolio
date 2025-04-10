0e4c51d14565d3a61dbb4ce53c6ed2af8f3f57ea
Duy Phạm <duypham@W3WMBP16A1044.local>
2025-04-10 16:59:24 +0700

    update resume routing to serve pdf from classpath resource

--- src/main/kotlin/com/helios/web/portfolio/routing/Resume.kt
+++ src/main/kotlin/com/helios/web/portfolio/routing/Resume.kt
@@ -1,14 +1,27 @@
 package com.helios.web.portfolio.routing
 
+import io.ktor.http.ContentDisposition
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.server.resources.get
-import io.ktor.server.response.respondFile
+import io.ktor.server.response.header
+import io.ktor.server.response.respondOutputStream
 import io.ktor.server.routing.Route
-import java.io.File
 
 fun Route.resume() {
     get<Resume> {
-        call.respondFile(
-            File("static/DuyPham_CV.pdf")
+        val inputStream = this::class.java.classLoader.getResourceAsStream("static/DuyPham_CV.pdf")
+            ?: throw IllegalStateException("Resume file not found")
+
+        call.response.header(
+            HttpHeaders.ContentDisposition,
+            ContentDisposition.Inline.withParameter(
+                ContentDisposition.Parameters.FileName,
+                "DuyPham_CV.pdf"
+            ).toString()
         )
+        call.respondOutputStream(ContentType.Application.Pdf) {
+            inputStream.copyTo(this)
+        }
     }
 }
